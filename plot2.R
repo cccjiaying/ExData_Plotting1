072564c00fbdebfded070d99346682c97a8d54e1
cccjiaying <cccjiaying@gmail.com>
2017-03-16 21:44:31 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table("~/Desktop/COURSERA/Exploratory Data Analysis/household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+plot(data$datetime,data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
